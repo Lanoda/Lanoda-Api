83664fca8579dc11c4f405bd6bc2149f37c5bcfc
IsaacVH <isaac.vanh@gmail.com>
2016-03-29 22:27:04 -0400

    Fixed up the Faker.

--- database/seeds/ContactsTableSeeder.php
+++ database/seeds/ContactsTableSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 use App\Contact;
 
 class ContactsTableSeeder extends Seeder
@@ -14,28 +13,30 @@ class ContactsTableSeeder extends Seeder
     public function run()
     {
 
-        $faker = Faker::create();
+        if (env('APP_ENV') == 'dev') {
+            $faker = Faker\Factory::create();
 
-        foreach (range(1, 30) as $index)
-        {
-            // Generate a name.
-        	$fullname = $faker->name(1);
-        	$arrayList = explode(' ', $fullname);
-        	$name = ['First', 'Last'];
-        	if (substr($arrayList[0], -1) == ".") {
-        		$name[0] = $arrayList[1];
-        		$name[1] = $arrayList[2];
-        	} else {
-        		$name[0] = $arrayList[0];
-        		$name[1] = $arrayList[1];
-        	}
+            foreach (range(1, 30) as $index)
+            {
+                // Generate a name.
+                $fullname = $faker->name(1);
+                $arrayList = explode(' ', $fullname);
+                $name = ['First', 'Last'];
+                if (substr($arrayList[0], -1) == ".") {
+                    $name[0] = $arrayList[1];
+                    $name[1] = $arrayList[2];
+                } else {
+                    $name[0] = $arrayList[0];
+                    $name[1] = $arrayList[1];
+                }
 
-            // Insert the Contact
-        	Contact::create([
-        		'firstname' => $name[0],
-        		'lastname' => $name[1],
-                'some_bool' => $faker->boolean(),
-        	]);
+                // Insert the Contact
+                Contact::create([
+                    'firstname' => $name[0],
+                    'lastname' => $name[1],
+                    'some_bool' => $faker->boolean(),
+                ]);
+            }
         }
     }
 }
