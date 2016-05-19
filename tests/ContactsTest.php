b3a7c24a2117296caba7a88c3753dd1bebc1449f
IsaacVH <isaac.vanh@gmail.com>
2016-05-19 01:10:58 -0400

    Opps, mistake in Test Case.

--- tests/ContactsTest.php
+++ tests/ContactsTest.php
@@ -6,7 +6,7 @@ class ContactsTest extends TestCase
 	public function testContactsGet () 
 	{
 		$response = $this->call('GET', '/contacts');
-		$this->assertEquals(200, $response->status())
+		$this->assertEquals(200, $response->status());
 	}
 }
 
