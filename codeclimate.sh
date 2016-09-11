9747343390a997d0ca75436eab6107a35fbab267
IsaacVH <isaac.vanh@gmail.com>
2016-09-11 15:28:02 -0400

    changed executable script.

--- codeclimate.sh
+++ codeclimate.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
+
 ./vendor/bin/test-reporter --stdout > codeclimate.json
 curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' https://codeclimate.com/test_reports
